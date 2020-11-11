54a38f3d563f700b412350f9637fd55379cbe1d5
MarcosValenzuela <marcosvalezuela@mail.fresnostate.edu>
2020-11-10 17:19:49 -0800

    commit psuedocode

--- lab04.py
+++ lab04.py
@@ -1,1 +1,19 @@
- 
+ """
+ main
+    question = "What is the capital of California? "
+    answer = "Sacramento"
+    ask(question, answer)
+
+ ask
+    tries = 0
+    loop three times
+        increment tries
+        ask user input
+        check user input is equal to answer
+            if so, print "Correct" then exit loop
+    if not correct
+        print to the user  "You have used up you all your guesses."
+        print the correct answer "The answer is 'Sacramento'"
+
+main
+"""
